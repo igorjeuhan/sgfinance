bdccd2763a944a1a777a57a725552f573c4e1fe0
igorjeuhan <igorjeuhan10@gmail.com>
2021-07-11 20:24:50 -0300

    add object

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import HeaderDash from './src/components/HeaderDash';
 import MenuDash from './src/components/MenuDash';
 import SectionColumn from './src/components/objects/SectionColumn';
 import SectionCards from './src/components/objects/SectionCards';
-import CardDash from './src/components/CardDash';
+import SubtitleDash from './src/components/SubtitleDash';
 
 //Importando o css do Settings
 import './src/css/generic/reset.css';
@@ -14,23 +14,31 @@ import './src/css/elements/base.css';
 import './src/css/elements/patternElements.css';
 import './src/css/elements/icons.css';
 
-
 //Montando o layout principal
 const $root = document.querySelector('#root');
 const $htmlHeaderDash = HeaderDash();
 const $htmlMenuDash = MenuDash();
 const $htmlSectionColumn = SectionColumn();
-const $htmlSectionCards = SectionCards();
-const $htmlCardDash = CardDash();
+const $htmlSubtitleDash = SubtitleDash();
+const $htmlSectionCards = SectionCards(4);
 
-$root.insertAdjacentHTML('beforeend', $htmlHeaderDash);
-$root.insertAdjacentHTML('beforeend', $htmlMenuDash);
-$root.insertAdjacentHTML('beforeend', $htmlSectionColumn);
+$root.insertAdjacentHTML(
+    "beforeend",
+    `
+        ${$htmlHeaderDash}
+        ${$htmlMenuDash}
+        ${$htmlSectionColumn}
+    ` 
+);
 
 //Incluindo componentes dentro de outros componentes
 const $sectionColumn = document.querySelector('#section-column');
-$sectionColumn.insertAdjacentHTML('beforeend', $htmlSectionCards);
-//Busca a seção dos cards e insere a quantidade de cards passando o valor por parâmetro
-const $sectionCards = document.querySelector('#section-cards');
-$sectionCards.insertAdjacentHTML('beforeend', $htmlCardDash.repeat(4));
+$sectionColumn.insertAdjacentHTML(
+    "beforeend", 
+    `
+        ${$htmlSubtitleDash}
+        ${$htmlSectionCards.repeat(2)}
+    `
+);
+
 
