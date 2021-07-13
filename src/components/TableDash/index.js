4ebf14495281b0116b51a606a0a425f3b992fd38
igorjeuhan <igorjeuhan10@gmail.com>
2021-07-12 21:06:56 -0300

    edit css table

--- src/components/TableDash/index.js
+++ src/components/TableDash/index.js
@@ -27,6 +27,16 @@ const TableDash = () => {
                                 <button class="btn-tb excluir"><img class="icon-dash" src="src/images/icons/delete.svg" alt="Excluir" title="Excluir"></button>
                             </td>
                         </tr>
+                        <tr>
+                            <td>Medicamentos controlados</td>
+                            <td>R$4.000,00</td>
+                            <td class="status-data"><img class="icon-dash" src="/src/images/icons/alert.svg" alt="Vencido" title="Vencido"></td>
+                            <td class="vencimento vencido">07/07/2021</td>
+                            <td>
+                                <button class="btn-tb editar"><img class="icon-dash" src="src/images/icons/edit.svg" alt="Editar" title="Editar"></button>
+                                <button class="btn-tb excluir"><img class="icon-dash" src="src/images/icons/delete.svg" alt="Excluir" title="Excluir"></button>
+                            </td>
+                        </tr>
                     </tbody>
                 </table>
         </section>
